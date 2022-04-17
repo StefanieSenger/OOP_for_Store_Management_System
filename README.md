bb91df34f58390fcbc15a7800270ff429145939c
Stefanie <stefanie.senger@posteo.de>
2022-04-17 12:07:39 +0200

    assigned attributes to self objects

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # OOP_for_Store_Management_System
 Following FreeCodeCamp's OOP Tutorial
 
+It's no got to have too much respect of classes and methods, especially since they're everywhere.
+
+The video I followed is here: https://www.youtube.com/watch?v=Ej_02ICOIgs
 The original repository can be found here: https://github.com/jimdevops19/PythonOOP
--- Store_Management_System.py
+++ Store_Management_System.py
@@ -1,18 +1,31 @@
+from cgi import print_exception
+
+
 class Item:
-    def __init__(self, name):
+
+    def __init__(self, name:str, price:float, quantity=0):
+        # Run validations to the received arguments
+        assert price >= 0, f"price {price} is not greater than or equal to zero!"
+        assert quantity >= 0, f"quantity {quantity} is not greater than or equal to zero!"
+
+        # Assign to self object
         self.name = name
-    def calculate_total_price(self, x, y):
-        return x * y
+        self.price = price
+        self.quantity = quantity
 
-item1 = Item("Phone")
-item1.price = 100
-item1.quantity = 5
-#print(item1.calculate_total_price(item1.price, item1.quantity))
+    def calculate_total_price(self):
+        return self.price * self.quantity
 
-item2 = Item("Laptop")
-item2.price = 1000
-item2.quantity = 3
-#print(item2.calculate_total_price(item2.price, item2.quantity))
+item1 = Item("Phone", 100, 1)
+
+item2 = Item("Laptop", 1000, 3)
+item2.has_numpad = False
 
 print(item1.name)
+print(item1.price)
+print(item1.quantity)
 print(item2.name)
+print(item2.price)
+print(item2.quantity)
+print(item2.has_numpad)
+print(item2.calculate_total_price())
